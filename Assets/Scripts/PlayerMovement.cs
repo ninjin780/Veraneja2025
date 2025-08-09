a0316040f8d278aaaada45b8f5a8373cce8ed929
David <43551883+ninjin780@users.noreply.github.com>
2025-08-09 23:33:42 +0200

    menu + comienzo movimiento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D playerRb;
+    public float speed;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(xInput) > 0)
+        {
+            playerRb.linearVelocity = new Vector2(xInput * speed, playerRb.linearVelocity.y);
+        }
+
+        if (Mathf.Abs(yInput) > 0)
+        {
+            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, yInput * speed);
+        }
     }
 }
