3fc86e95897042934b8a19739619d7466bd0c80e
David <43551883+ninjin780@users.noreply.github.com>
2025-08-14 19:37:54 +0200

    mover hacia atras

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
+    private Vector3 position;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            position = collision.gameObject.GetComponent<Transform>().localPosition;
             Debug.Log("Damage");
-            collision.gameObject.GetComponent<PlayerCombat>().RemoveLife(3);
+            collision.gameObject.GetComponent<PlayerCombat>().RemoveLife(3); 
+            if (position.x >= 0)
+            {
+                position = new Vector3(position.x - 25, position.y, 1);
+            }
+            else
+            {
+                position = new Vector3(position.x + 25, position.y, 1); 
+            }
+            
         }
     }
 
