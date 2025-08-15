6fe5e93ebfdeb35dcd39ac1072f33d2b959521e7
David <43551883+ninjin780@users.noreply.github.com>
2025-08-15 16:12:25 +0200

    escupitajo

--- Assets/Scripts/Player/Interponer Animaciones.cs
+++ Assets/Scripts/Player/Interponer Animaciones.cs
@@ -9,18 +9,20 @@ public class InterponerAnimaciones : MonoBehaviour
     private bool ataque = false;
     private AnimationClip currentClip;
     private AnimationClip nextClip;
+    private float xInput;
+    
+    void Update()
+    {
+        CheckInput();
+    }
+    void CheckInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+    }
     private void FixedUpdate()
     {
-        if (animator1.transform.position.x < 0)
-        {
-            animator2.transform.localScale = new Vector3(animator1.transform.localScale.x, animator1.transform.localScale.y, 1);
-            animator2.transform.position = new Vector3(animator2.transform.position.x + 3, animator2.transform.position.y, 1);
-        }
-        else if (animator1.transform.position.x > 0)
-        {
-            animator2.transform.localScale = new Vector3(-animator1.transform.localScale.x, animator1.transform.localScale.y, 1);
-            animator2.transform.position = new Vector3(animator2.transform.position.x - 3, animator2.transform.position.y, 1);
-        }
+        animator2.transform.localScale = new Vector3(-0.25f, 0.25f, 1);
+        animator2.transform.position = new Vector3(animator1.transform.position.x * xInput, animator1.transform.position.y, 1);
         info1 = animator1.GetCurrentAnimatorClipInfo(0);
         currentClip = info1[0].clip;
         if (currentClip.name == "Attack")
